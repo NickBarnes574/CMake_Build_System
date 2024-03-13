71ed962ddd8ca4ce28d8199bcf17fcc8c364c0b3
Nick Barnes <nick.a.barnes44@gmail.com>
2024-03-13 07:43:35 -0400

    work on testing

--- cmake/testing/add_all_tests.cmake
+++ cmake/testing/add_all_tests.cmake
@@ -1,15 +1,21 @@
 # FILENAME: add_all_tests.cmake
 # -----------------------------------------------------------------------------
 # DESCRIPTION: Defines a function to add test suites for each target in the 
-# project. This script is responsible for integrating unit tests, possibly 
-# using GTest or other testing frameworks.
+# project. This script is responsible for integrating unit tests.
 # -----------------------------------------------------------------------------
 
 # Function to add test suites for each target
 function(add_all_tests)
-    # Here you can loop through each target and add corresponding tests
-    # This is a placeholder and should be adapted based on your project structure
-    # Example: add_gtest(MyExecutable tests/my_executable_tests.cpp)
+# Assuming all tests link against 'my_project_lib' and potentially 'other_dependency_lib'
+set(COMMON_LIBRARIES my_project_lib other_dependency_lib)
+
+file(GLOB TEST_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/tests/*.c")
+
+foreach(TEST_SOURCE ${TEST_SOURCES})
+    get_filename_component(TEST_NAME ${TEST_SOURCE} NAME_WE)
+    # Now calling add_single_test with COMMON_LIBRARIES passed through ${ARGN}
+    add_single_test(${TEST_NAME} ${TEST_SOURCE} ${COMMON_LIBRARIES})
+endforeach()
 endfunction()
 
 # *** end of file ***
